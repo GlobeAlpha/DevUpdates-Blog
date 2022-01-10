82cd314d44b421b11738a4a48813a64eb4b09c76
MaciekB <maciekbajda92@hotmail.com>
2022-01-10 19:00:40 +0000

    Upload from my local directory

---
+++ devupdates/src/App.js
@@ -0,0 +1,42 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './index.css';
+import PostList from './components/PostList.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FirstSprint from "./components/Sprint_Docs/Sprint_Planning_Session_1.js";
+import SecondSprint from "./components/Sprint_Docs/Sprint_Planning_Session_2.js";
+import ThirdSprint from "./components/Sprint_Docs/Sprint_Planning_Session_3.js";
+import FourthSprint from "./components/Sprint_Docs/Sprint_Planning_Session_4.js";
+import Retro_1 from "./components/Sprint_Docs/Retro_1 .js";
+import Retro_2 from "./components/Sprint_Docs/Retro_2.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>Dev Updates Blog by Maciek Bajda</header>
+ 
+      <Router>
+         <Routes >
+      
+      {/*Display Posts */}
+     <Route path="/" element ={<PostList />} />
+               {/* <Route path="/Project_Intro" element ={<ProjectIntro />} /> */}
+          
+              <Route path="/First_Sprint" element={<FirstSprint />} />
+             <Route path="/Second_Sprint" element={<SecondSprint />} />
+          <Route path="/First_Retro" element={<Retro_1 />} />
+          
+          <Route path="/Third_Sprint" element ={<ThirdSprint />} />
+          <Route path="//Fourth_Sprint" element={<FourthSprint />} />
+          <Route path="/Second_Retro" element ={<Retro_2 />} />
+         
+        </Routes >
+      </Router>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ devupdates/src/components/Sprint_Docs/Sprint_Planning_Session_2.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Post   from "../Post.js";
+
+export default class SecondSprint extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+
+        const SprintSession2Data =
+        <div>
+            <h1>First Paragraph</h1>
+           <h2>Second Paragraph</h2>
+            <h3> Third Paragraph</h3>
+            <h4>Fourth Paragraph</h4>
+            </div>
+        
+        return (
+            <Post Title="Project Intro" Description="This is a introductary post to new Prroject " body={SprintSession2Data} />
+        );
+    };
+
+}
+
+
+
+       {/* <Post Title="Project Intro" Description="This is a introductary post to new Prroject " body ={SprintSession4Data} /> */}
+
+         {/* Second  Retro */}
+
+      {/* <Post Title="Project Intro" Description="This is a introductary post to new Prroject " body={Retro2} /> */}
